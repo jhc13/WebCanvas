729a19584136a0af5ae9a714fbb8ee1b907e67d3
jhc13 <39209141+jhc13@users.noreply.github.com>
2025-01-01 23:09:04 -0500

    Fix chat history processing for Gemini

--- agent/LLM/gemini.py
+++ agent/LLM/gemini.py
@@ -4,6 +4,8 @@ import asyncio
 from functools import partial
 import multiprocessing
 from concurrent.futures import ThreadPoolExecutor
+
+from google.ai.generativelanguage_v1beta import Part
 from sanic.log import logger
 import google.generativeai as genai
 
@@ -26,12 +28,21 @@ class GeminiGenerator:
     def chat(self, messages, max_tokens=500, temperature=0.7):
         chat_history = []
         for message in messages:
-            chat_history.append({"role": "user", "parts": [{"text": message.get("content")}]})
-            # chat_history.append({"role": "model", "parts": [{"text": message.get("content")}]})
+            content = message['content']
+            if isinstance(content, str):
+                parts = [Part(text=content)]
+            else:
+                # It is a list of dictionaries with keys `type` and `text`.
+                parts = []
+                for content_part in content:
+                    if content_part['type'] == 'text':
+                        parts.append(Part(text=content_part['text']))
+            chat_history.append({'role': 'user', 'parts': parts})
+        last_message = chat_history.pop()
         running_model = genai.GenerativeModel(self.model)
         chat = running_model.start_chat(history=chat_history)
-        latest_user_message = messages[-1].get("content")
-        response = chat.send_message(latest_user_message, generation_config=genai.types.GenerationConfig(
-            max_output_tokens=max_tokens,
-            temperature=temperature))
+        response = chat.send_message(
+            last_message, generation_config=genai.types.GenerationConfig(
+                max_output_tokens=max_tokens,
+                temperature=temperature))
         return response.text
